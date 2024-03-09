ed6a0b792e50d4f23a864d0b44c0a4bbd34dd953
anqit <anqit.praqash@gmail.com>
2024-03-08 21:26:45 -0700

    add map

--- lib/src/serializer/serializer.dart
+++ lib/src/serializer/serializer.dart
@@ -4,6 +4,8 @@ typedef JsonSerializer<To> = Serializer<dynamic, To>;
 
 extension SerializerExtensions<F, T> on Serializer<F, T> {
   Serializer<List<F>, List<T>> many() => (fs) => fs.map(this).toList();
+
+  Serializer<F, T2> map<T2>(T2 Function(T) mapper) => (f) => mapper(this(f));
 }
 
 extension JsonSerializerExtensions<To> on JsonSerializer<To> {

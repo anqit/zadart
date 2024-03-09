fb9f8b045ce659b3adb8bb089e7ee58341899613
anqit <anqit.praqash@gmail.com>
2024-03-08 21:33:25 -0700

    contramap

--- lib/src/serializer/serializer.dart
+++ lib/src/serializer/serializer.dart
@@ -3,11 +3,9 @@ typedef Serializer<From, To> = To Function(From);
 typedef JsonSerializer<To> = Serializer<dynamic, To>;
 
 extension SerializerExtensions<F, T> on Serializer<F, T> {
-  Serializer<List<F>, List<T>> many() => (fs) => fs.map(this).toList();
+  Serializer<List<F>, List<T>> forList() => (fs) => fs.map(this).toList();
 
   Serializer<F, T2> map<T2>(T2 Function(T) mapper) => (f) => mapper(this(f));
-}
 
-extension JsonSerializerExtensions<To> on JsonSerializer<To> {
-  JsonSerializer<List<To>> forList() => (fs) => fs.map(this).toList();
+  Serializer<F2, T> contraMap<F2>(F Function(F2) contraMapper) => (f) => this(contraMapper(f));
 }

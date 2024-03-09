30228bc05f7918a80f1054b5f91ab18fa316a550
anqit <anqit.praqash@gmail.com>
2024-03-08 20:36:47 -0700

    json serializer

--- lib/src/serializer/serializer.dart
+++ lib/src/serializer/serializer.dart
@@ -5,3 +5,7 @@ typedef JsonSerializer<To> = Serializer<dynamic, To>;
 extension SerializerExtensions<F, T> on Serializer<F, T> {
   Serializer<List<F>, List<T>> many() => (fs) => fs.map(this).toList();
 }
+
+extension JsonSerializerExtensions<To> on JsonSerializer<To> {
+  JsonSerializer<List<To>> forList() => (fs) => fs.map(this).toList();
+}

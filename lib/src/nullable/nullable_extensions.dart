213f2c277d9accf5bddf01e3a7ff109fa07f6206
anqit <anqit.praqash@gmail.com>
2024-08-01 00:53:38 -0600

    update ifNotNull to chain-able

--- lib/src/nullable/nullable_extensions.dart
+++ lib/src/nullable/nullable_extensions.dart
@@ -2,7 +2,15 @@ extension ZadartNullableExtensions<E> on E? {
   B? map<B>(B? Function(E) f) =>
       this != null ? f(this as E) : null;
 
-  void ifNotNull(void Function(E) f) => map(f);
+  E? ifNotNull(void Function(E) f, { void Function()? orElse }) {
+    if (this != null) {
+      f(this as E);
+      return this;
+    } else {
+      if (orElse != null) orElse();
+      return null;
+    }
+  }
 
   B? inverse<B>(B? ifNull) =>
       this == null ? ifNull : null;

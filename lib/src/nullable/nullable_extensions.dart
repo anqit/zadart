1ba545dfcfe1819c997abe7de488fd72659c9be5
anqit <anqit.praqash@gmail.com>
2024-08-22 19:32:53 -0600

    lazy and async nullable extensions

--- lib/src/nullable/nullable_extensions.dart
+++ lib/src/nullable/nullable_extensions.dart
@@ -2,6 +2,9 @@ extension ZadartNullableExtensions<E> on E? {
   B? map<B>(B? Function(E) f) =>
       this != null ? f(this as E) : null;
 
+  Future<B?> asyncMap<B>(Future<B?> Function(E) f) async =>
+      this != null ? await f(this as E) : null;
+
   E? ifNotNull(void Function(E) f, { void Function()? orElse }) {
     if (this != null) {
       f(this as E);
@@ -12,9 +15,25 @@ extension ZadartNullableExtensions<E> on E? {
     }
   }
 
+  Future<E?> asyncIfNotNull(Future<void> Function(E) f, { Future<void> Function()? orElse }) async {
+    if (this != null) {
+      await f(this as E);
+      return this;
+    } else {
+      if (orElse != null) await orElse();
+      return null;
+    }
+  }
+
   B? inverse<B>(B? ifNull) =>
       this == null ? ifNull : null;
 
+  B? lazyInverse<B>(B? Function() ifNull) =>
+      this == null ? ifNull() : null;
+
   E? filter(bool Function(E) p) =>
       this != null && p(this as E) ? this : null;
+
+  Future<E?> asyncFilter(Future<bool> Function(E) p) async =>
+      this != null && await p(this as E) ? this : null;
 }
